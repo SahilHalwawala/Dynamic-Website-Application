7d312b8beae0fcee9e54c96a68f7ae38572a8b9b
SahilHalwawala <halwawalasahil0@gmail.com>
2024-10-27 23:14:04 -0400

    Add files via upload

---
+++ GradeService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentGradesApplication.Entities;
+
+namespace StudentGradesApplication.Services
+{
+    //public class GradeService : IGradeService - This helps link both page
+    public class GradeService : IGradeService
+    {
+        private readonly StudentGradesDbContext _context;
+
+        public GradeService(StudentGradesDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Course> GetCourses()
+        {
+            return _context.Courses.ToList();
+        }
+
+
+        public List<Grade> GetAllGrades()
+        {
+            return _context.Grades.Include(g => g.Course).ToList();
+        }
+
+        public int AddGrade(Grade grade)
+        {
+            _context.Grades.Add(grade);
+            _context.SaveChanges();
+
+            return grade.GradeId;
+        }
+
+        public Grade GetStudentGrade(int gradeId)
+        {
+            return _context.Grades.Find(gradeId);
+        }
+
+        public int UpdateGrade(Grade grade)
+        {
+            _context.Grades.Update(grade);
+            _context.SaveChanges();
+
+            return grade.GradeId;
+        }
+
+        public int DeleteGrade(Grade grade)
+        {
+            _context.Grades.Remove(grade);
+            _context.SaveChanges();
+
+            return grade.GradeId;
+        }
+
+    }
+}
---
+++ IGradeService.cs
@@ -0,0 +1,19 @@
+﻿using StudentGradesApplication.Entities;
+namespace StudentGradesApplication.Services
+{
+    public interface IGradeService
+    {
+        public List<Course> GetCourses();
+        public List<Grade> GetAllGrades();
+
+        public Grade GetStudentGrade(int gradeId);
+
+        public int AddGrade(Grade grade);
+
+        public int UpdateGrade(Grade grade);
+
+        public int DeleteGrade(Grade gradeId);
+
+
+    }
+}
